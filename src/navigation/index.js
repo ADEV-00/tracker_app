115c7580bff2de184e881dfbf99bfdd416302865
unknown <developer.amel@gmail.com>
2020-11-13 21:51:52 +0100

    ?

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,8 +1,4 @@
-import {
-  NavigationContainer,
-  DefaultTheme,
-  DarkTheme,
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as React from "react";
 import SignInScreen from "../screens/SignInScreen";
