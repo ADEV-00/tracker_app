94df978b5b72a6b1e4ec4f35cf857b58cbc8cd65
unknown <developer.amel@gmail.com>
2020-11-30 17:35:49 +0100

    Focus recording on other screen

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -8,15 +8,18 @@ import { useIsFocused } from "@react-navigation/native";
 import TrackForm from "../components/TrackForm";
 
 const TrackCreateScreen = () => {
-  const { state, addLocation } = useContext(LocationContext);
+  const {
+    state: { recording },
+    addLocation,
+  } = useContext(LocationContext);
   const callback = useCallback(
     (location) => {
-      addLocation(location, state.recording);
+      addLocation(location, recording);
     },
-    [state.recording]
+    [recording]
   );
   const isFocused = useIsFocused();
-  const [err] = useLocation(isFocused, callback);
+  const [err] = useLocation(isFocused || recording, callback);
 
   return (
     <View>
