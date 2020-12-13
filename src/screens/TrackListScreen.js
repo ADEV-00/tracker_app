f4acbe8ba00800bfd36fb7a023a59fc34260e8ce
unknown <developer.amel@gmail.com>
2020-12-13 22:14:38 +0100

    Track List styling added

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -2,6 +2,10 @@ import React, { isValidElement, useContext, useEffect } from "react";
 import { View, Text, StyleSheet, Button, FlatList } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import { Context as TrackContext } from "../context/TrackContext";
+import {
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native-gesture-handler";
 
 const TrackListScreen = ({ navigation }) => {
   const { state, fetchTracks } = useContext(TrackContext);
@@ -15,23 +19,56 @@ const TrackListScreen = ({ navigation }) => {
   }, [navigation]);
 
   return (
-    <View>
-      <Text>Track List Screen</Text>
+    <View style={{ backgroundColor: "#2C2B44", flex: 1 }}>
+      {/*   <Text>Track List Screen</Text>
       <Button
         title="Track Detail"
         onPress={() => navigation.navigate("TrackDetail")}
-      />
+      /> */}
       <FlatList
         data={state}
         keyExtractor={(item) => item._id}
         renderItem={({ item }) => {
-          return <Text>{item.name}</Text>;
+          return (
+            <View style={styles.itemwrapper}>
+              <Text>Icon</Text>
+              <Text style={styles.itemTitle}>{item.name}</Text>
+              <TouchableOpacity style={styles.btnMore}>
+                <Text style={{ fontSize: 16, color: "#01ABC7" }}>More</Text>
+              </TouchableOpacity>
+            </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  itemwrapper: {
+    width: "90%",
+    alignSelf: "center",
+    backgroundColor: "#252539",
+    marginBottom: 20,
+    height: 60,
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  itemTitle: {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "bold",
+  },
+  btnMore: {
+    borderWidth: 1,
+    borderColor: "#01ABC7",
+    paddingHorizontal: 8,
+    paddingVertical: 3,
+    borderRadius: 3,
+  },
+});
 
 export default TrackListScreen;
