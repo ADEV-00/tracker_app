7710929fa50561c3cbe1017d0c1481a0bcf25b6d
unknown <developer.amel@gmail.com>
2020-12-15 14:29:28 +0100

    Added styling to bottom tab bar

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -4,15 +4,62 @@ import * as React from "react";
 import AccountScreen from "../screens/AccountScreen";
 import TrackCreateScreen from "../screens/TrackCreateScreen";
 import TrackListScreen from "../screens/TrackListScreen";
+import { FontAwesome, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
 const BottomTab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
   return (
-    <BottomTab.Navigator initialRouteName="TrackCreate">
-      <BottomTab.Screen name="TrackList" component={TrackListNavigator} />
-      <BottomTab.Screen name="TrackCreate" component={TrackCreateNavigator} />
-      <BottomTab.Screen name="Acount" component={AcountNavigator} />
+    <BottomTab.Navigator
+      initialRouteName="TrackCreate"
+      tabBarOptions={{
+        style: {
+          backgroundColor: "#2C2B44",
+          height: "8%",
+          borderTopColor: "transparent",
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 6,
+          },
+          shadowOpacity: 0.39,
+          shadowRadius: 8.3,
+
+          elevation: 13,
+        },
+        activeTintColor: "#3AEBF6",
+      }}
+    >
+      <BottomTab.Screen
+        name="TrackList"
+        component={TrackListNavigator}
+        options={{
+          tabBarLabel: "Tracks",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="th-list" color="#3AEBF6" size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        options={{
+          tabBarLabel: "Add Track",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="plus" color="#3AEBF6" size={size} />
+          ),
+        }}
+        name="TrackCreate"
+        component={TrackCreateNavigator}
+      />
+      <BottomTab.Screen
+        options={{
+          tabBarLabel: "Account",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="user-alt" color="#3AEBF6" size={size} />
+          ),
+        }}
+        name="Acount"
+        component={AcountNavigator}
+      />
     </BottomTab.Navigator>
   );
 }
